76bba1968b9e3d75f0df33e04343a83d1433377f
ErenBGT <76560719+erenbgt@users.noreply.github.com>
2024-05-21 23:42:59 +0300

    Drops Items from Inventory V1.0

---
+++ TheLonelyAlchemistV1.0/Assets/AlertDialogManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AlertDialogManager : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public TMP_Text messageText;
+    public Button okButton;
+    public Button cancelButton;
+
+    private System.Action<bool> responseCallBack;
+
+    private void Start()
+    {
+        dialogBox.SetActive(false);
+
+        okButton.onClick.AddListener(() => HandleResponse(true));
+        cancelButton.onClick.AddListener(() => HandleResponse(true));
+    }
+
+    public void ShowDilaog(string message, System.Action<bool> callback)
+    {
+        responseCallBack = callback;
+        messageText.text = message;
+        dialogBox.SetActive(true);
+    }
+
+    private void HandleResponse(bool response)
+    {
+        dialogBox.SetActive(false);
+        responseCallBack?.Invoke(response);
+    }
+
+
+
+
+}
--- TheLonelyAlchemistV1.0/Assets/ScriptsBGT/DragDrop.cs
+++ TheLonelyAlchemistV1.0/Assets/ScriptsBGT/DragDrop.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -43,7 +44,7 @@ public class DragDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
     public void OnDrag(PointerEventData eventData)
     {
         //So the item will move with our mouse (at same speed)  and so it will be consistant if the canvas has a different scale (other then 1);
-        
+
         rectTransform.anchoredPosition += eventData.delta;
 
     }
@@ -52,11 +53,34 @@ public class DragDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        GameObject tempItemReference = itemBeingDragged;
 
         itemBeingDragged = null;
 
         if (transform.parent == startParent || transform.parent == transform.root)
         {
+            //Item Drop to world
+            tempItemReference.SetActive(false);
+
+            AlertDialogManager dialogManager = FindAnyObjectByType<AlertDialogManager>();
+
+            dialogManager.ShowDilaog("Do you want to drop this item?", (response) =>
+            {
+                if (response)
+                {
+                    DropItemIntoTheWorld(tempItemReference);
+                }
+                else
+                {
+                    transform.position = startPosition;
+                    transform.SetParent(startParent);
+
+                    tempItemReference.SetActive(true);
+                }
+            });
+
+
+
             transform.position = startPosition;
             transform.SetParent(startParent);
 
@@ -67,6 +91,21 @@ public class DragDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
         canvasGroup.blocksRaycasts = true;
     }
 
+    private void DropItemIntoTheWorld(GameObject tempItemReference)
+    {
+        string cleanName = tempItemReference.name.Split(new string[] { "(Clone)" }, StringSplitOptions.None)[0];
 
+        GameObject item = Instantiate(Resources.Load<GameObject>(cleanName + "_Model"));
 
+        item.transform.position = Vector3.zero;
+        Vector3 dropSpawnPosition = PlayerState.Instance.playerBody.transform.Find("DropSpawn").transform.position;
+        item.transform.localPosition = new Vector3(dropSpawnPosition.x, dropSpawnPosition.y, dropSpawnPosition.z);
+
+        //var itemsObject = FindAnyObjectByType<EnvironmentManager>().gameObject.transform.Find("[Items]");
+        //item.transform.SetParent(itemsObject.transform);
+
+        DestroyImmediate(tempItemReference.gameObject);
+        InventorySystem.Instance.ReCalculateList();
+        CraftingSystem.Instance.RefreshNeededItems();
+    }
 }
